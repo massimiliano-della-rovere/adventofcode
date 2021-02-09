24024fd5fefb898b85fa9c9458f04fadc533c408
Massimiliano della Rovere <massimiliano.dellarovere@gmail.com>
2021-02-09 14:13:42 +0100

    corrected 4th test

--- 2020/_04/main.py
+++ 2020/_04/main.py
@@ -10,7 +10,13 @@ HERE = pathlib.Path(__file__).parent
 INPUT_FILE_NAME = "input.txt"
 
 
-LENGTH_SPLITTER = re.compile(r"^([0-9]+)(in|cm)$").search
+LENGTH_SPLITTER = re.compile(r"^(\d+)(in|cm)$").search
+
+
+Tester = typing.Callable[[str], bool]
+Passport = typing.Dict[str, str]
+Regulations = typing.Dict[str, Tester]
+Engine = typing.Callable[[Passport, Regulations], bool]
 
 
 def byr_validator(value: str) -> bool:
@@ -51,47 +57,41 @@ def iyr_validator(value: str) -> bool:
     return len(value) == 4 and 2010 <= int(value) <= 2020
 
 
-REQUIRED_FIELDS = {
+REGULATIONS = {
     "byr": byr_validator,
     "iyr": iyr_validator,
     "eyr": eyr_validator,
     "hgt": hgt_validator,
     "hcl": re.compile(r"^#[0-9a-f]{6}$").search,
     "ecl": ecl_validator,
-    "pid": re.compile(r"^(0*[0-9]+){9}$").search}
+    "pid": re.compile(r"^\d{9}$").search
+}
 
 
-def key_tester(current_passport: typing.Dict[str, str],
-               required_keys: typing.Dict[
-                   str,
-                   typing.Sequence[typing.Callable[[str], bool]]]) -> bool:
+def key_tester(current_passport: Passport,
+               required_keys: Regulations) -> bool:
     return set(required_keys) <= set(current_passport)
 
 
-def value_tester(current_passport: typing.Dict[str, str],
-                 required_keys: typing.Dict[
-                     str,
-                     typing.Callable[[str], bool]]) -> bool:
+def value_tester(current_passport: Passport,
+                 required_keys: Regulations) -> bool:
     for key, tester in required_keys.items():
         try:
             value = current_passport[key]
         except KeyError:
-            print(f"missing {key} in {current_passport}")
+            # print(f"missing {key} in {current_passport}")
             return False
         else:
             if not tester(value):
-                print(f"failed {tester.__name__}/{key}={value}"
-                      f" in {current_passport}")
+                # print(
+                #     f"failed {tester.__name__}/{key}={value}"
+                #     f" in {current_passport}")
                 return False
     else:
         return True
 
 
-def solve(test_method: typing.Callable[
-            [
-                typing.Dict[str, str],
-                typing.Dict[str, typing.Callable[[str], bool]]
-            ], bool]) -> int:
+def solve(test_method: Engine) -> int:
     valid_passports = 0
     current_passport = {}
     with open(HERE / INPUT_FILE_NAME) as f:
@@ -103,10 +103,10 @@ def solve(test_method: typing.Callable[
                     pair.split(":")
                     for pair in line.split(" "))
             else:
-                if test_method(current_passport, REQUIRED_FIELDS):
+                if test_method(current_passport, REGULATIONS):
                     valid_passports += 1
                 current_passport.clear()
-        if test_method(current_passport, REQUIRED_FIELDS):
+        if test_method(current_passport, REGULATIONS):
             valid_passports += 1
         return valid_passports
 
