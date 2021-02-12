7c91b51265e2837b021e7a08df0ec1d6db7893d9
Massimiliano della Rovere <massimiliano.dellarovere@gmail.com>
2021-02-12 19:23:15 +0100

    14th day wip

---
+++ 2020/day14/main.py
@@ -0,0 +1,31 @@
+"""14th day of advent of code 2020."""
+
+
+import abc
+import dataclasses
+import enum
+import math
+import operator
+import pathlib
+import typing
+
+
+HERE = pathlib.Path(__file__).parent
+INPUT_FILE_NAME = "input.txt"
+
+
+def solve1() -> int:
+    pass
+
+
+def solve2() -> int:
+    pass
+
+
+def main():
+    print(solve1())
+    print(solve2())
+
+
+if __name__ == "__main__":
+    main()
