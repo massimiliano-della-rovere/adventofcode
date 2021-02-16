4f23a6796b15bb3a00ed2b9ed60e69868fcbc1e7
Massimiliano della Rovere <massimiliano.dellarovere@gmail.com>
2021-02-16 14:38:22 +0100

    16th day

--- 2020/day16/main.py
+++ 2020/day16/main.py
@@ -2,9 +2,10 @@
 
 
 import collections
+import functools
 import itertools
+import operator
 import pathlib
-import re
 import typing
 
 
@@ -26,12 +27,12 @@ Tickets = typing.Sequence[Ticket]
 
 
 def import_rule(line: str) -> Rule:
-    name, ranges = line.split(": ")[1]
+    name, intervals = line.split(": ")
     return Rule(
         name,
         tuple(
             tuple(int(value) for value in interval.split("-"))
-            for interval in ranges.split(" or ")))
+            for interval in intervals.split(" or ")))
 
 
 def import_ticket(line: str) -> Ticket:
@@ -51,58 +52,70 @@ def get_values() -> dict[str, typing.Union[Rules, Ticket]]:
             else:
                 key, importer = next(engine)
                 f.readline()
+    ret["my ticket"] = ret["my ticket"][0]
     return {k: tuple(v) for k, v in ret.items()}
 
 
-def solve1() -> int:
+def solve1() -> tuple[int, dict[str, typing.Union[Rules, Ticket]]]:
     data = get_values()
     rules = data["rules"]
-    invalid = []
+    invalid_values = []
+    valid_tickets = []
+    invalid_tickets = []
     for ticket in data["nearby tickets"]:
         for value in ticket:
             if not any(
                     min_value <= value <= max_value
                     for rule in rules
-                    for min_value, max_value in rule):
-                invalid.append(value)
+                    for min_value, max_value in rule.ranges):
+                invalid_values.append(value)
+                invalid_tickets.append(ticket)
                 break
-    return sum(invalid)
+        else:
+            valid_tickets.append(ticket)
+    data["valid tickets"] = tuple(valid_tickets)
+    data["invalid tickets"] = tuple(invalid_tickets)
+    return sum(invalid_values), data
 
 
-def get_valid_tickets(rules: Rules,
-                      tickets: Tickets) -> typing.Generator[Ticket, None, None]:
-    for ticket in tickets:
-        if any(
-                min_value <= value <= max_value
-                for value in ticket
-                for rule in rules
-                for min_value, max_value in rule):
-            yield ticket
+def solve2(data: dict[str, typing.Union[Rules, Ticket]]) -> int:
+    rules = data["rules"]
+    possible_matches = collections.defaultdict(set)
+    tickets = tuple(
+        itertools.chain(data["valid tickets"], (data["my ticket"],)))
+    for part_index in range(len(data["my ticket"])):
+        for rule_id, rule in enumerate(rules):
+            if all(
+                    any(min_value <= ticket[part_index] <= max_value
+                        for min_value, max_value in rule.ranges)
+                    for ticket in tickets
+            ):
+                possible_matches[part_index].add(rule_id)
 
+    guessed = {}
+    while possible_matches:
+        removed = {
+            possible_matches.pop(part_id).pop(): part_id
+            for part_id, rule_ids in list(possible_matches.items())
+            if len(rule_ids) == 1}
+        guessed.update(removed)
+        for rule_id in removed:
+            for part_ids in possible_matches.values():
+                part_ids.remove(rule_id)
 
-# def match(ticket: Ticket, rules: Rules) -> typing.TypedDict[int, list[int]]:
-
-
-
-
-# def solve2() -> int:
-#     data = get_values()
-#     rules = data["rules"]
-#     part_rule_match = {
-#         part_index: list(range(len(rules)))
-#         for part_index, rule in enumerate(data["my ticket"])
-#     }
-#     for valid_ticket in get_valid_tickets(rules, data["nearby tickets"]):
-#         for part_index, part in enumerate(valid_ticket):
-#             if len(part_rule_match[part_index]) > 1:
-#                 part_rule_match[part_index] = [
-#
-#                 ]
+    return functools.reduce(
+        operator.mul,
+        (
+            data["my ticket"][guessed[rule_index]]
+            for rule_index, rule in enumerate(rules)
+            if rule.name.startswith("departure")
+        ))
 
 
 def main():
-    print(solve1())
-    # print(solve2())
+    result, data = solve1()
+    print(result)
+    print(solve2(data))
 
 
 if __name__ == "__main__":
